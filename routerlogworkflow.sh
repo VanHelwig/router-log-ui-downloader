19b7794ebfe1052e98932659de66b1f3de70aaaa
VanHelwig <93016865+VanHelwig@users.noreply.github.com>
2024-08-30 14:58:16 -0500

    Update routerlogworkflow.sh

--- routerlogworkflow.sh
+++ routerlogworkflow.sh
@@ -1,4 +1,7 @@
 #!/bin/bash 
 
+# runs python script to download router logs
+# then runs bash script to transfer the files to desired location locally
+
 /home/user/Scripts/Python/downloadrouterlogs.py
 sudo /home/user/Scripts/Bash/routerlogtransfer.sh
