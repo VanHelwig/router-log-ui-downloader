6f2374f032d979f7033302970c5dfa5af4b2e03f
VanHelwig <93016865+VanHelwig@users.noreply.github.com>
2024-10-20 13:47:15 -0500

    Update routerlogtransfer.sh

--- routerlogtransfer.sh
+++ routerlogtransfer.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-#sets source and destination directories
-SOURCE_DIR=/home/user/Downloads
-DEST_DIR=/var/log/routerlogs
+# Sets source and destination directories (customize these paths)
+SOURCE_DIR="$HOME/Downloads"  # Default to user's Downloads directory
+DEST_DIR="/var/log/routerlogs"  # Default destination for router logs (customizable)
 
-# loops for router syslog files is source directory
-for file in $(ls $SOURCE_DIR/syslog-*); do
-        # checks if there files that match the name pattern
-        if [ -e $file ]; then
-                # copies syslog files to destination directory if matched
-                mv -v $file $DEST_DIR
-        fi
+# Loops through router syslog files in the source directory
+for file in $(ls $SOURCE_DIR/syslog-* 2>/dev/null); do
+	# Checks if there are files that match the name pattern
+	if [ -e "$file" ]; then
+		# Moves syslog files to the destination directory if matched
+		mv -v "$file" "$DEST_DIR"
+	fi
 done
